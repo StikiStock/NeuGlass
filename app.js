010c6414f7fbaf94f5554cc2d21f9e8199d3719c
Stiki Stock <mamastiki7@gmail.com>
2022-10-14 11:15:45 +0330

    animated the card movement using flip technique
    plus a new gradient image on the main index

--- app.js
+++ app.js
@@ -19,7 +19,78 @@ animated.forEach((el) => observer.observe(el));
 
 
 let showMenu = () => {
-    document.querySelector('.Arrow').classList.toggle('rotate-[270deg]')
-    document.querySelector('.Arrow').classList.toggle('rotate-[90deg]')
     document.querySelector('.Menu').classList.toggle('hidden')
+}
+
+
+let tartib = 0;
+
+let animateOut = () => {
+  
+  tartib++
+
+  const Card = document.querySelector('.Card');
+  const Arrow = document.querySelector('.Arrow');
+  const Menu = document.querySelector('.Menu');
+
+  const first = Card.getBoundingClientRect();
+
+  showMenu();
+
+  const last = Card.getBoundingClientRect();
+
+
+  const deltaX = first.left - last.left;
+  const deltaY = first.top - last.top;
+  const deltaW = first.width / last.width;
+  const deltaH = first.height / last.height;
+
+
+  Card.animate([{
+  transformOrigin: 'top left',
+  transform: `
+    translate(${deltaX}px, ${deltaY}px)
+    scale(${deltaW}, ${deltaH})
+    `
+  }, {
+    transformOrigin: 'top left',
+    transform: 'none'
+  }], {
+    duration: 300,
+    easing: 'ease-in-out',
+    fill: 'both'
+  });
+
+  Menu.animate([{
+    transform: 'translateX(-5px)',
+    opacity: '0'
+  }, {
+    transform: 'translateX(0px)',
+    opacity: '1'
+  }], {
+    duration: 400,
+    easing: 'ease-in-out',
+    fill: 'both'
+  })
+
+  if (tartib%2 == 0) {
+    Arrow.animate([{
+      transform: 'rotate(270deg)' 
+    }], {
+      duration: 300,
+      easing: 'ease-in-out',
+      fill: 'both'
+    });
+  }
+
+  else if (tartib%2 == 1) {
+    Arrow.animate([{
+      transform: 'rotate(90deg)' 
+    }], {
+      duration: 300,
+      easing: 'ease-in-out',
+      fill: 'both'
+    });
+  }
+
 }
